7f7e29c417bb4494cbbca837b16256e98e5c0cc9
Dmitrii <sokolov.di@inbox.ru>
2021-03-27 22:55:23 +0300

    Initial commit

---
+++ src/sample/Main.java
@@ -0,0 +1,19 @@
+/**
+ * 1. Создать окно для клиентской части чата: большое текстовое поле для отображения переписки в центре окна.
+ * Однострочное текстовое поле для ввода сообщений и кнопка для отсылки сообщений на нижней панели.
+ * Сообщение должно отсылаться либо по нажатию кнопки на форме, либо по нажатию кнопки Enter.
+ * При «отсылке» сообщение перекидывается из нижнего поля в центральное.
+ */
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+
+}
